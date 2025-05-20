f342c8f4be5909577f029f2f2020a7fe6a41546a
BRUNO DAL PRÁ <bruno_pra@estudante.sesisenai.org.br>
2025-05-20 09:50:50 -0300

    Acho que ta pronto

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
 import express from 'express'
+import fs from 'fs'
 
- /* const express = require('express')
- updatiar o package.json se utilizar import express from 'express', adicionando
- "type": "module" */
+/* const express = require('express')
+  atualizar o package.json se utilizar import express from 'express', adicionando
+  "type": "module"
+*/
 
+/* servidor pode ser inicializado com "node (nome de arquivo [server.js])",
+  para o servidor atualizar as mudanças no código automaticamente, nodemon deve estar
+  inicializado no start (que também deve ser criado no arquivo package.json), e 
+  o comando se torna "node start"
+*/
 
- /* servidor pode ser inicializado com "node (nome de arquivo [server.js])",
- para o servidor atualizar as mudanças no código automaticamente, nodemon deve estar
- inicializado no start (que também deve ser criado no arquivo package.json), e 
- o comando se torna "node start" */
-
-    //linha 23 -> caminho, transformação, callback
+//linha 23 -> caminho, transformação, callback
 
 //Inicializando o Servidor.
-
-
 const server = express()
 server.use(express.json())
 
@@ -25,57 +25,131 @@ server.options('/', (req, res)=>{
 })
 
 server.get('/tarefas', (req, res)=> {
-    fs.readFile('./banco.json', (err, data) =>{
-        if(err){
-            res.status(500).json({erro:err})
-        }
-        res.status(200).json(data)
-    })
-})
-    //listar UMA tarefa especifica, caso nao existir, retornar 404
-    server.get('/tarefs/:id', (req, res)=> {
-        const tarefa_id = req.params.id
-        let msg = ''
-        let cod_err = 200
-            // ler o arquivo banco.json
     fs.readFile('./banco.json', 'utf-8', (err, data) =>{
         if(err){
             res.status(500).json({erro:err})
-        }
-        const lista_de_tarefas = JSON.parse(data)
-        const tarefa = lista_de_tarefas.find((tarefa) => tarefa.id == tarefa_id)
-        if (!tarefa) {
-            res.status(404).json({err: "Item não encontrado!"})
         } else {
-            res.send('ok')
+            const tarefas = JSON.parse(data)
+            res.status(200).json(tarefas)
         }
-        res.status(cod_err).json({msg:msg})
-        // procurar pela tarefa de ID informado (.find)
     })
+})
 
-    // ler o arquivo banco.json
-    // procurar pela tarefa de ID informado (.find)
-    // retornar 404 caso nao ache
-    // retornar 200 e os dados da tarefa caso ache
+//listar UMA tarefa especifica, caso nao existir, retornar 404
+server.get('/tarefas/:id', (req, res)=> {
+    const tarefa_id = req.params.id
+    fs.readFile('./banco.json', 'utf-8', (err, data) =>{
+        if(err){
+            res.status(500).json({erro:err})
+        } else {
+            const lista_de_tarefas = JSON.parse(data)
+            const tarefa = lista_de_tarefas.find((tarefa) => tarefa.id == tarefa_id)
+            if (!tarefa) {
+                res.status(404).json({err: "Item não encontrado!"})
+            } else {
+                res.status(200).json(tarefa)
+            }
+        }
     })
+})
 
-    //criar uma tarefa
-    server.post('/tarefas', (req, res) => {
-        const {titulo, descricao, dificuldades, user_id} = req.body
-        console.log(titulo, descricao, dificuldade, user)
-        res.send('ok')
-    })
+//criar uma tarefa
+server.post('/tarefas', (req, res) => {
+    const {titulo, descricao, dificuldades, user_id} = req.body
     //pegar as informações da nova tarefa, do body da requisição
+
     //lendo meu arquivo (readFile)
-    //inserir a nova tarefa, no arquivo
-    //salvar o arquivo (writeFile, apendFile)
+    fs.readFile('./banco.json', 'utf-8', (err, data) => {
+        if(err){
+            res.status(500).json({erro:err})
+        } else {
+            const lista = JSON.parse(data)
+            const novaTarefa = {
+                id: Date.now().toString(), // cria um ID único com timestamp
+                titulo,
+                descricao,
+                dificuldades,
+                user_id
+            }
 
-    //atualizar uma tarefa
-    //deletar uma tarefa
+            //inserir a nova tarefa no arquivo
+            lista.push(novaTarefa)
 
-    //Criar uma CRUD em um arquivo json utilizando o Restful.
+            //salvar o arquivo (writeFile)
+            fs.writeFile('./banco.json', JSON.stringify(lista, null, 2), (err) => {
+                if(err){
+                    res.status(500).json({erro:err})
+                } else {
+                    res.status(201).json(novaTarefa)
+                }
+            })
+        }
+    })
+})
 
-    //Mandar o servidor ouvir na porta 8000.
+//atualizar uma tarefa
+server.put('/tarefas/:id', (req, res) => {
+    const tarefa_id = req.params.id
+    const {titulo, descricao, dificuldades, user_id} = req.body
 
-server.listen(8000, () => {console.log('Servidor rodando na porta 8000')})
+    fs.readFile('./banco.json', 'utf-8', (err, data) => {
+        if(err){
+            res.status(500).json({erro:err})
+        } else {
+            let lista = JSON.parse(data)
+            const index = lista.findIndex(t => t.id == tarefa_id)
 
+            if(index === -1){
+                res.status(404).json({erro: "Tarefa não encontrada"})
+            } else {
+                // atualizar os dados
+                lista[index] = {
+                    ...lista[index],
+                    titulo,
+                    descricao,
+                    dificuldades,
+                    user_id
+                }
+
+                fs.writeFile('./banco.json', JSON.stringify(lista, null, 2), (err) => {
+                    if(err){
+                        res.status(500).json({erro:err})
+                    } else {
+                        res.status(200).json(lista[index])
+                    }
+                })
+            }
+        }
+    })
+})
+
+//deletar uma tarefa
+server.delete('/tarefas/:id', (req, res) => {
+    const tarefa_id = req.params.id
+
+    fs.readFile('./banco.json', 'utf-8', (err, data) => {
+        if(err){
+            res.status(500).json({erro:err})
+        } else {
+            let lista = JSON.parse(data)
+            const novaLista = lista.filter(t => t.id != tarefa_id)
+
+            if(lista.length === novaLista.length){
+                res.status(404).json({erro: "Tarefa não encontrada"})
+            } else {
+                fs.writeFile('./banco.json', JSON.stringify(novaLista, null, 2), (err) => {
+                    if(err){
+                        res.status(500).json({erro:err})
+                    } else {
+                        res.status(204).send()
+                    }
+                })
+            }
+        }
+    })
+})
+
+//Mandar o servidor ouvir na porta 8000.
+server.listen(8000, () => {
+    console.log('Servidor rodando na porta 8000')
+})
